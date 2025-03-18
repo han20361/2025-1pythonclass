3d4a4987922020412a0a5c4e69aee991afc653f7
user <rohj2005@naver.com>
2025-03-18 12:14:54 +0900

    퀴즈

--- 02list.py
+++ 02list.py
@@ -1,5 +1,8 @@
 # 2025.03.18
 # 파이썬 리스트: 한개의 변수에 여러 값을 할당
+from dask.order import order
+from datashader import first
+from sympy.physics.units import second
 
 # 인덱싱
 colors = ['red', 'blue', 'green']
@@ -10,4 +13,40 @@ print(len(colors)) # 리스트 길이 출력
 
 # 슬라이싱
 cities = ['서울', '부산', '인천', '의정부', '대전', '강릉', '논산', '포항']
-print(cities[0:8])
+print(cities[0:5]) # cities[0:n] 0~ n-1까지 표시
+print(cities[:7]) # 0~6번째 원소
+print(cities[:-1]) # 0~ -2까지
+print(cities[3:]) # 전부 표시
+print(cities[-4:]) # 뒤에서부터 4번째 표시
+print(cities[:7:2]) # 처음부터 6번째 원소까지, 한칸씩 건너뜀
+print(cities[::3]) # 처음부터 끝까지, 세칸씩 건너뜀
+print(cities[::-1]) # 처음부터 끝까지, 거꾸로
+print(cities[4::-2])
+
+# 리스트의 추가
+colors = ['red', 'blue', 'green']
+colors.append('white')
+print(colors[:])
+colors.extend(['black', 'purple'])
+print(colors[:])
+colors.insert(1, 'orange')
+print(colors[:])
+colors.remove('purple')
+print(colors[:])
+colors[1] = 'sky'
+print(colors[:])
+
+# 패킹, 언패킹
+c1, c2, c3, _, _, _ = colors
+print(c1, c2, c3)
+
+# 퀴즈
+first = ["egg", "salad", " bread", "soup", "canafe"]
+second = ["fish", "lamb", "pork", "beef", "chicken"]
+third = ["apple", "banana", "orange", "grape", "mango"]
+
+order = [first, second, third]
+john = [order[0][:-2], second[1::3], third[0]]
+del john[2]
+john.extend(order[2][0:1])
+print(john)
