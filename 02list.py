dac2043227514adbca918dc5648c376ac1389338
user <rohj2005@naver.com>
2025-03-18 11:38:41 +0900

    인덱싱, 슬라이싱

---
+++ 02list.py
@@ -0,0 +1,13 @@
+# 2025.03.18
+# 파이썬 리스트: 한개의 변수에 여러 값을 할당
+
+# 인덱싱
+colors = ['red', 'blue', 'green']
+print(colors) # 리스트 전체 출력
+print(colors[1]) # 리스트 두번째 원소 출력, 0부터 시작
+print(colors[-1]) # 리스트 마지막 원소 출력
+print(len(colors)) # 리스트 길이 출력
+
+# 슬라이싱
+cities = ['서울', '부산', '인천', '의정부', '대전', '강릉', '논산', '포항']
+print(cities[0:8])
