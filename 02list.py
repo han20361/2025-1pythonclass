1b61c5b3684ead8933848677199df637c45aadc6
user <rohj2005@naver.com>
2025-03-18 12:19:24 +0900

    퀴즈1

--- 02list.py
+++ 02list.py
@@ -47,6 +47,6 @@ third = ["apple", "banana", "orange", "grape", "mango"]
 
 order = [first, second, third]
 john = [order[0][:-2], second[1::3], third[0]]
-del john[2]
-john.extend(order[2][0:1])
+print(john)
+john.extend((order[2][0:1]))
 print(john)
